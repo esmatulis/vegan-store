4b1e9539874a1f9c234088ce0544917d1b3d2111
Erica Matulis <erica.matulis2@gmail.com>
2025-03-29 11:56:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add event listener to change navbar background on scroll
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
